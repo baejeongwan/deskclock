ede581fc93bfe3bcfd0a9011009f87de026d3b43
baejeongwan <jayden.bae@outlook.kr>
2023-02-04 17:44:50 +0900

    Changes to 1.2.0

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-const { app, BrowserWindow, Menu, ipcMain } = require('electron')
+const { app, BrowserWindow, Menu, ipcMain, dialog } = require('electron')
 const path = require('path')
+const { autoUpdater } = require('electron-updater')
+const ProgressBar = require('electron-progressbar')
 const NewsAPI = require('newsapi')
 
-require('dotenv').config()
+let newsapi;
+let progressBar;
+require('dotenv').config({path: path.join(__dirname, ".env")})
 
+try {
+    newsapi = new NewsAPI(process.env.NEWSAPIKEY)
+    
+} catch (error) {
+    console.error("ERROR DETECTED WHILE LOADING NEWSAPI... JUST IGNORING...")
+}
 
-const newsapi = new NewsAPI(process.env.NEWSAPIKEY)
+autoUpdater.autoDownload = false;
+autoUpdater.checkForUpdates()
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -49,4 +60,30 @@ ipcMain.handle("get-news-data", async (event) => {
         country: "kr",
         pageSize: 5
     })
+})
+
+autoUpdater.addListener("update-available", info => {
+    let returnNum = dialog.showMessageBoxSync({message: "Update to the version " + info.version + " is available!\nWould you like to download?", buttons: ["Yes, download now", "No, skip for now"], title: "Update available!"})
+    if (returnNum == 0) {
+        autoUpdater.downloadUpdate()
+        var progressBar = new ProgressBar({
+            indeterminate: false,
+            text: "Downloading updates...",
+            detail: "Wait...",
+            maxValue: 100
+        })
+    }
+    
+})
+
+autoUpdater.addListener("download-progress", progress => {
+    progressBar.value = progress.percent
+})
+
+autoUpdater.addListener("update-downloaded", info => {
+    progressBar.close()
+    let returnNum = dialog.showMessageBoxSync({message: "Update to the version " + info.version + " is downloaded!\nWould you like to install now?", buttons: ["Yes, install now", "No, do on exit"], title: "Update available!"})
+    if (returnNum == 0) {
+        autoUpdater.quitAndInstall()
+    }
 })
--- renderer.js
+++ renderer.js
@@ -1,10 +1,20 @@
 const webviewModal = new bootstrap.Modal($("#webviewModal"))
+const settingsModal = new bootstrap.Modal($("#settingsModal"))
+const appOffcanvas = new bootstrap.Offcanvas($("#appOffcanvas"))
+
+const apps = [
+    {
+        "appId": "settings",
+        "modalObj": settingsModal,
+        "displayName": `<i class="bi bi-gear-fill"></i> Settings`
+    }
+]
 
 window.datareader.getNewsData()
 
 setInterval(() => {
     let now = new Date()
-    if (now.getHours() < 10) {
+    if (now.getMinutes() < 10) {
         $("#main-clock").innerText = now.getHours() + " : 0" + now.getMinutes()
     } else {
         $("#main-clock").innerText = now.getHours() + " : " + now.getMinutes()
@@ -13,11 +23,37 @@ setInterval(() => {
 }, 500);
 
 
+
+function $(query) {
+    return document.querySelector(query)
+}
+
+//#region Webview
+
 function openAtWebview(source) {
     $("#webviewModalWebview").src = source
     webviewModal.show()
 }
 
-function $(query) {
-    return document.querySelector(query)
+$("#webviewModal").addEventListener('hidden.bs.modal', event => {
+    $("#webviewModalWebview").src = "about:blank"
+})
+
+//#endregion
+
+//#region App Management
+loadApps()
+function loadApps() {
+    $("#appListPanel").innerHTML = "";
+    apps.forEach((element, index) => {
+        $("#appListPanel").innerHTML += `<button type="button" class="list-group-item list-group-item-action" onclick="openApp(${index})">${element.displayName}</button>`
+    })
 }
+
+function openApp(idNum) {
+    let appToOpen = apps[idNum]
+    appOffcanvas.hide()
+    appToOpen.modalObj.show()
+}
+
+//#endregion
